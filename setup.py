d59d057401f6153f13d268ce3705b6cde20457ca
l-vincent-l <lara.vincent@gmail.com>
2016-04-19 10:50:57 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
+
+def is_pkg(line):
+    return line and not line.startswith(('--', 'git', '#'))
+
+with open('requirements.txt', encoding='utf-8') as reqs:
+    install_requires = [l for l in reqs.read().split('\n') if is_pkg(l)]
+
 setup(
   name = 'messenger_hook',
   packages = ['messenger_hook'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'A library to create hooks for facebook messenger',
   author = 'Vincent Lara',
   author_email = 'lara.vincent@gmail.com',
@@ -10,4 +17,8 @@ setup(
   download_url = 'https://github.com/l-vincent-l/messenger_hook/tarball/0.1', # I'll explain this in a second
   keywords = ['facebook', 'messenger'], # arbitrary keywords
   classifiers = [],
+  extras_require = {
+      'falcon': ['falcon']
+  },
+  install_requires=install_requires
 )
